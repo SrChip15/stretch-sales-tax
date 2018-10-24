f3dfe557e8daa9d84b5ab0c86ba8d52aca0a1ab2
Sri Nath <sri.developer731@gmail.com>
2018-10-24 23:48:34 +0000

    Sales tax exercise completed

--- st.js
+++ st.js
@@ -1,32 +1,77 @@
-let salesTaxRates = {
-  AB: 0.05,
-  BC: 0.12,
-  SK: 0.10
+let taxes = {
+  rates: {
+    AB: 0.05,
+    BC: 0.12,
+    SK: 0.10
+  },
+
+  computeTaxes: function (province, totalSales) {
+    return this.rates[province] * totalSales;
+  }
 };
 
-let companySalesData = [
-  {
+let companySalesData = [{
     name: "Telus",
     province: "BC",
-    sales: [ 100, 200, 400 ]
+    sales: [100, 200, 400]
   },
   {
     name: "Bombardier",
     province: "AB",
-    sales: [ 80, 20, 10, 100, 90, 500 ]
+    sales: [80, 20, 10, 100, 90, 500]
   },
   {
     name: "Telus",
     province: "SK",
-    sales: [ 500, 100 ]
+    sales: [500, 100]
   }
 ];
 
-function calculateSalesTax(salesData, taxRates) {
-  // Implement your code here
+// make new property name 'totalSales' in companySalesData object (side-effect)
+function aggregateSales() {
+  for (let company in companySalesData) {
+    let totalSales = 0;
+    // console.log(`${company} -> ${companySalesData[company].name}`);
+
+    companySalesData[company].sales.forEach(sales => {
+      totalSales += sales;
+    });
+    companySalesData[company].totalSales = totalSales;
+  }
 }
 
-var results = calculateSalesTax(companySalesData, salesTaxRates);
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+  let telus = {};
+  let bombardier = {};
+
+  aggregateSales();
+
+  for (var company in salesData) {
+    let name = salesData[company].name; 
+    let totalSales = salesData[company].totalSales;
+
+    let totalTaxes = taxes.computeTaxes(salesData[company].province, totalSales);
+
+    if (name === 'Telus') {
+      telus.totalSales = telus.totalSales == undefined ? totalSales : telus.totalSales += totalSales;
+      telus.totalTaxes = telus.totalTaxes == undefined ? totalTaxes : telus.totalTaxes += totalTaxes;
+    } else {
+      bombardier.totalSales = bombardier.totalSales == undefined ? totalSales : bombardier.totalSales += totalSales;
+      bombardier.totalTaxes = bombardier.totalTaxes == undefined ? totalTaxes : bombardier.totalTaxes += totalTaxes;
+    }
+  }
+
+  // console.log({telus, bombardier});
+  return {telus, bombardier};
+}
+
+console.log(calculateSalesTax(companySalesData));
+
+
+// console.log(taxes.computeTaxes('AB', 1000)); WORKS
+// console.log(companySalesData);
+// var results = calculateSalesTax(companySalesData, salesTaxRates);
 
 /* Expected Results:
 {
